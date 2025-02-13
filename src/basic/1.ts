546cd20377a68e6aa49f72af75be6e3a8093c403
Mariia Savchenko <mashasavchenko@yahoo.com>
2025-02-13 23:06:25 +0200

    specified return types

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -2,7 +2,7 @@ const age: number = 50;
 const toggle: boolean = true;
 const username: string = "Max";
 const empty: null = null;
-export const callback = (a: number) => {
+export const callback = (a: number): number => {
   return 100 + a;
 };
 export const data1 = { age, toggle, username, empty };
--- src/basic/4.ts
+++ src/basic/4.ts
@@ -1,8 +1,8 @@
-function showMessage(message: string) {
+function showMessage(message: string): string {
   return message;
 }
 
-function calc(num1: number, num2: number) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
